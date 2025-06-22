a99600ae233796e37043d8cacb2f8a8cf370cccc
Artyom Gabtraupov <artyom.gabtraupov@liftwerx.com>
2025-06-21 20:22:40 -0400

    models

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,7 +2,21 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class Script(BaseModel):
+    # _id: ObjectId(str)
     name: str
+    description: str
+    # created_at: datetime
+    # updated_at: datetime
+    path: str
+    class Parameter(BaseModel):
+        # _id: ObjectId(str)
+        name: str
+        type: str
+        description: str
+
+    parameters: list[Parameter]
+
     class Config:
         # allow extra fields
         extra = "allow"
+        
